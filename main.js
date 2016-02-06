c8abaa3e34757f9db599eaf7c9936c9cd7b2df8b
Chris <chris@sourcelair.com>
2016-02-07 01:27:46 +0200

    javascript for change div content

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-function myFunction() {
-  alert("yo");
-  //document.getElementById('2').innerHTML = "Hello World";
+function myFunction(x) {
+  existing = typeof existing !== 'undefined' ? existing : 1;
+  //alert(x + "+" + existing);
+  document.getElementById(existing).style.display = 'none';
+  document.getElementById(x).style.display = 'block';
+  existing = x;
 }
